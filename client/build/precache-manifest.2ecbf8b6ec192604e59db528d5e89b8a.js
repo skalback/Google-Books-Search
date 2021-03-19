52c205e521eae15b05db3a7a8172c1b2f2535ac3
skalback <70301513+skalback@users.noreply.github.com>
2021-03-19 01:23:08 -0400

    json updates

---
+++ client/build/precache-manifest.2ecbf8b6ec192604e59db528d5e89b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62128d47247cae5885b99dda00ba1088",
+    "url": "/static/media/background.62128d47.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "382c0d5a26dccdc1d4a3",
+    "url": "/static/js/main.cb9c8950.chunk.js"
+  },
+  {
+    "revision": "6d4b4ec2a630ed201b49",
+    "url": "/static/js/2.9098beb6.chunk.js"
+  },
+  {
+    "revision": "382c0d5a26dccdc1d4a3",
+    "url": "/static/css/main.75b3e023.chunk.css"
+  },
+  {
+    "revision": "49bd6fc2bf04a44eaf017aa702ece95a",
+    "url": "/index.html"
+  }
+];
